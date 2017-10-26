aa6ebfd6d77070caaf0b5942b8ae76d79b6b525d
Stefan <psa9710@gmail.com>
2017-10-26 22:07:04 +0300

    HOME UI + DIALOG
    Removed Label (was forgoten to be deleted)
    Made Content Move Vertically when Snackbar apears in dialogbox
    Fixed Error in SnackbarMessage
    Code Simplified

--- LibraryServer/Home.xaml.cs
+++ LibraryServer/Home.xaml.cs
@@ -36,14 +36,14 @@ namespace LibraryServer
 
 
         }
-
+        #region Time_Related_Methods
         private void LabelTextChangeRelatedToTime()
         {
             int time = DateTime.Parse(DateTime.Now.ToString("HHtt")).Hour;
             if (time < 12) LabelGreeting.Content = "Good Morning,";
             else if (time < 17) LabelGreeting.Content = "Good Afternoon,";
             else if (time < 24) LabelGreeting.Content = "Good Evening,";
-            
+
         }
 
         //functie ce updateaza timpul in aplicatie
@@ -52,20 +52,22 @@ namespace LibraryServer
             //Set content of Label to current time in a HH:MM format
             LabelTimer.Content = DateTime.Now.ToString("HH:mm");
             tickCount++;
-            lbl.Content = tickCount;
         }
+        #endregion
+        #region SnackBarDisplays
+        //Recieves a string and time to display, and pops a snackbarnotification with the recieved string, and time of displayment
 
-        //Recieves a string, and pops a snackbarnotification with the recieved string, and time of displayment
-        private void SnackbarMessageDisplay(string s,int _timeSpan)
-        {   
-            if (tickCount < 2+(_timeSpan/1000)) return;
+        //There's a issue with Snackbars poping a shor period of time , so i had to set a timeinterval
+        private void SnackbarMessageDisplay(string s, int _timeSpan)
+        {
+            if (tickCount < 2 + (_timeSpan / 1000)) return;
             ////SnackbarMaximumCharacters.IsActive = true;
             //var messageQueue = SnackbarMaximumCharacters.MessageQueue;
 
             //tickCount = 0;//reset tick count
             ////find out what this shit does
             //Task.Factory.StartNew(() => messageQueue.Enqueue(s));
-            var message = "Maximum character  is " + MaximumTextCharacters.ToString() + "!";
+            // var message = "Maximum character  is " + MaximumTextCharacters.ToString() + "!";
             //var messageQueue = SnackbarDialogHost.MessageQueue;
             ////find out what this shit does
             //Task.Factory.StartNew(() => messageQueue.Enqueue(message));
@@ -73,36 +75,47 @@ namespace LibraryServer
 
             var messageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(_timeSpan));
             SnackbarMaximumCharacters.MessageQueue = messageQueue;
-            SnackbarMaximumCharacters.MessageQueue.Enqueue(message);
+            SnackbarMaximumCharacters.MessageQueue.Enqueue(s);
             tickCount = 0;
         }
 
+        private void SnackBarDialogHostMessageDisplay(string s, int _timeSpan)
+        {  
+            if (tickCount < 1 + _timeSpan/1000) return;
+            
+
+                //var message = "Maximum character  ce  is " + MaximumTextCharacters.ToString() + "!";
+                //var messageQueue = SnackbarDialogHost.MessageQueue;
+                ////find out what this shit does
+                //Task.Factory.StartNew(() => messageQueue.Enqueue(message));
+
+
+                var messageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(_timeSpan));
+                SnackbarDialogHost.MessageQueue = messageQueue;
+                SnackbarDialogHost.MessageQueue.Enqueue(s);
+                tickCount = 0;
+
+        }
+        #endregion
+        #region HOME_UI
+        #region TextBoxNameInput
         private void TextBoxNameInput_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (TextBoxNameInput.Text.Length == MaximumTextCharacters)
             {
                 //if one of the following keys is pressed, ignore the event
-                if (!(e.Key == System.Windows.Input.Key.Delete ||
-                    e.Key == System.Windows.Input.Key.Back ||
-                    e.Key == System.Windows.Input.Key.Left ||
-                    e.Key == System.Windows.Input.Key.Right ||
-                    e.Key == System.Windows.Input.Key.A && Keyboard.Modifiers == ModifierKeys.Control ||
-                    e.Key == Key.V && Keyboard.Modifiers == ModifierKeys.Control ||
-                    e.Key == Key.C && Keyboard.Modifiers == ModifierKeys.Control ||
-                    e.Key == Key.X && Keyboard.Modifiers == ModifierKeys.Control ||
-                    e.Key == Key.End || e.Key == Key.Home || e.Key == Key.Tab || e.Key == Key.Enter
-                    ))
+                if (!GetPressedKey(e))
                 {
                     e.Handled = true;
                     var message = "Maximum character limit is " + MaximumTextCharacters.ToString() + "!";
-                    SnackbarMessageDisplay(message,1500);
+                    SnackbarMessageDisplay(message, 1500);
                 }
             }
             if (e.Key == Key.Enter)
             {
                 if (TextBoxNameInput.Text == "")
                 {
-                    SnackbarMessageDisplay("A username is required in order to login",1500);
+                    SnackbarMessageDisplay("A username is required in order to login", 1500);
                     e.Handled = true;
                 }
                 //fire the clickbutton in order to pop DialogBox;
@@ -110,6 +123,7 @@ namespace LibraryServer
                 //B/*uttonRaiseDialog.Command.Execute(ButtonRaiseDialog.CommandParameter);*/
             }
         }
+
         // if text(has changed& has more than the maximumtextcharacters, reset and display message
         private void TextBoxNameInput_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -117,29 +131,20 @@ namespace LibraryServer
             {
                 TextBoxNameInput.Text = "";
                 var message = "Maximum character limit is " + MaximumTextCharacters.ToString() + "! The name you tried to enter exceeds the allowed limit!";
-                SnackbarMessageDisplay(message,1500);
+                SnackbarMessageDisplay(message, 1500);
             }
         }
+        #endregion
+
+
+        #endregion
+        #region DialogHost
+        #region DialogHost_FiredEvents
         //when DialogHostLogIn is opened, execute the following code
         private void DialogHostLogIn_DialogOpened(object sender, DialogOpenedEventArgs eventArgs)
         {
             TextBoxUserName.Text = TextBoxNameInput.Text;
             //Keyboard.Focus(PasswordBoxUserPassword);
-        }
-
-        private void ButtonCancelDialogHost_Click(object sender, RoutedEventArgs e)
-        {
-            AcceptButtonIsPressed = false;
-            ButtonCancelDialogHost.Command.Execute(null);
-
-        }
-
-
-        private void ButtonAcceptDialogHost_Click(object sender, RoutedEventArgs e)
-        {
-            AcceptButtonIsPressed = true;
-            ButtonAcceptDialogHost.Command.Execute(null);
-
         }
 
         //When te DIalogHostLogIn is closing, execute the followiing code
@@ -149,22 +154,86 @@ namespace LibraryServer
             if (AcceptButtonIsPressed)
             {
                 var message = "Succesfully logged in!";
-                SnackbarMessageDisplay(message,2000);
+                SnackbarMessageDisplay(message, 2000);
             }
             else
             {
                 TextBoxNameInput.Text = ""; //reset TextBoxNameInput
                 var message = "\"Cancel\" was pressed by the user";
-                SnackbarMessageDisplay(message,2000);
+                SnackbarMessageDisplay(message, 2000);
             }
             AcceptButtonIsPressed = false; //make sure it's false, whatever it happens
+        }
+
+
+        #endregion
+        #region DialogHost_Content
+        private void ButtonAcceptDialogHost_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBoxUserPassword.Password != "")
+            {
+                AcceptButtonIsPressed = true;
+                ButtonAcceptDialogHost.Command.Execute(null);
+            }
+            else
+            {
+
+            }
+
+        }
+
+        private void ButtonCancelDialogHost_Click(object sender, RoutedEventArgs e)
+        {
+            AcceptButtonIsPressed = false;
+            ButtonCancelDialogHost.Command.Execute(null);
+
         }
 
         private void TextBoxUserName_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (TextBoxUserName.Text.Length > MaximumTextCharacters)
             {
-                if (!(e.Key == System.Windows.Input.Key.Delete ||
+                if (!GetPressedKey(e))
+                {
+                    e.Handled = true;
+                    SnackBarDialogHostMessageDisplay("Character limit is" + MaximumTextCharacters.ToString() + "!", 1000);
+
+                }
+            }
+        }
+
+        private void PasswordBoxUserPassword_PreviewKeyDown(object sender, KeyEventArgs e)
+        {   
+            if (PasswordBoxUserPassword.Password.Length == 13)
+            {
+                if (GetPressedKey(e)) return;
+                SnackBarDialogHostMessageDisplay("Maximum Password length is 13!", 1000);
+
+            }
+            //If the pressed buttons are not numbers or special cases , error 
+            if (!GetPressedKey(e) && !(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                e.Handled = true;
+                var message = "Please insert only numbers";
+                SnackBarDialogHostMessageDisplay(message, 1000);
+
+            }
+
+
+        }
+        #endregion
+        #endregion
+
+        #region ExtraFunctions
+        /// <summary>
+        /// This function tests if a specific Key combination has been Pressed
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private bool GetPressedKey(KeyEventArgs e)
+        {
+            if (
+                    e.Key == System.Windows.Input.Key.Delete ||
                     e.Key == System.Windows.Input.Key.Back ||
                     e.Key == System.Windows.Input.Key.Left ||
                     e.Key == System.Windows.Input.Key.Right ||
@@ -172,32 +241,16 @@ namespace LibraryServer
                     e.Key == Key.V && Keyboard.Modifiers == ModifierKeys.Control ||
                     e.Key == Key.C && Keyboard.Modifiers == ModifierKeys.Control ||
                     e.Key == Key.X && Keyboard.Modifiers == ModifierKeys.Control ||
-                    e.Key == Key.End || e.Key == Key.Home || e.Key == Key.Tab || e.Key == Key.Enter
-                    ))
-                {
-                    e.Handled = true;
-                    //Because of knwon Issue, can not be called multiple times in short timeSpan
-                    if (tickCount>2)
-                    {
-                        
-                        var message = "Maximum character  is " + MaximumTextCharacters.ToString() + "!";
-                        //var messageQueue = SnackbarDialogHost.MessageQueue;
-                        ////find out what this shit does
-                        //Task.Factory.StartNew(() => messageQueue.Enqueue(message));
-
-
-                        var messageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(1000));
-                        SnackbarDialogHost.MessageQueue = messageQueue;
-                        SnackbarDialogHost.MessageQueue.Enqueue(message);
-                        tickCount = 0;
-                    }
-                }
-            }
+                    e.Key == Key.End || e.Key == Key.Home ||
+                    (e.Key == Key.End && Keyboard.Modifiers == ModifierKeys.Shift ||
+                    e.Key == Key.Home && Keyboard.Modifiers == ModifierKeys.Shift) ||
+                    e.Key == Key.Tab ||
+                    e.Key == Key.LeftCtrl || e.Key == Key.RightCtrl ||
+                    e.Key == Key.LeftShift || e.Key == Key.RightShift
+                    ) return true;
+            return false;
         }
+#endregion
 
-        private void PasswordBoxUserPassword_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
     }
 }
