f3678c1071a86ab6dee0f66f7e73bd517adf8630
Stefan <psa9710@gmail.com>
2017-10-26 13:23:21 +0300

    Dialog
    Added Dialog
    Placed Content
    Resized & Rescaled Content
    Added Events for various items
    Added DialogSnackbar
    Snackbar issue at quick presses, fix found
    Modified general Snackbar as well to recieve time for snackbar

--- LibraryServer/Home.xaml.cs
+++ LibraryServer/Home.xaml.cs
@@ -19,6 +19,8 @@ namespace LibraryServer
     {
         #region Local_Variables
         int MaximumTextCharacters = 15;//the amount of characters a user is allowed to input
+        bool AcceptButtonIsPressed = false;
+        int tickCount = 0; //check ticks in order to Call SnacbkarEnque Known ISSUE #738
         #endregion
 
 
@@ -41,7 +43,7 @@ namespace LibraryServer
             if (time < 12) LabelGreeting.Content = "Good Morning,";
             else if (time < 17) LabelGreeting.Content = "Good Afternoon,";
             else if (time < 24) LabelGreeting.Content = "Good Evening,";
-
+            
         }
 
         //functie ce updateaza timpul in aplicatie
@@ -49,17 +51,30 @@ namespace LibraryServer
         {
             //Set content of Label to current time in a HH:MM format
             LabelTimer.Content = DateTime.Now.ToString("HH:mm");
+            tickCount++;
+            lbl.Content = tickCount;
         }
 
-        //Recieves a string, and pops a snackbarnotification with the recieved string
-        private void SnackbarMessageDisplay(string s)
-        {
-            //SnackbarMaximumCharacters.IsActive = true;
-            var messageQueue = SnackbarMaximumCharacters.MessageQueue;
+        //Recieves a string, and pops a snackbarnotification with the recieved string, and time of displayment
+        private void SnackbarMessageDisplay(string s,int _timeSpan)
+        {   
+            if (tickCount < 2+(_timeSpan/1000)) return;
+            ////SnackbarMaximumCharacters.IsActive = true;
+            //var messageQueue = SnackbarMaximumCharacters.MessageQueue;
+
+            //tickCount = 0;//reset tick count
+            ////find out what this shit does
+            //Task.Factory.StartNew(() => messageQueue.Enqueue(s));
+            var message = "Maximum character  is " + MaximumTextCharacters.ToString() + "!";
+            //var messageQueue = SnackbarDialogHost.MessageQueue;
+            ////find out what this shit does
+            //Task.Factory.StartNew(() => messageQueue.Enqueue(message));
 
 
-            //find out what this shit does
-            Task.Factory.StartNew(() => messageQueue.Enqueue(s));
+            var messageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(_timeSpan));
+            SnackbarMaximumCharacters.MessageQueue = messageQueue;
+            SnackbarMaximumCharacters.MessageQueue.Enqueue(message);
+            tickCount = 0;
         }
 
         private void TextBoxNameInput_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -75,19 +90,19 @@ namespace LibraryServer
                     e.Key == Key.V && Keyboard.Modifiers == ModifierKeys.Control ||
                     e.Key == Key.C && Keyboard.Modifiers == ModifierKeys.Control ||
                     e.Key == Key.X && Keyboard.Modifiers == ModifierKeys.Control ||
-                    e.Key == Key.End || e.Key == Key.Home
+                    e.Key == Key.End || e.Key == Key.Home || e.Key == Key.Tab || e.Key == Key.Enter
                     ))
                 {
                     e.Handled = true;
                     var message = "Maximum character limit is " + MaximumTextCharacters.ToString() + "!";
-                    SnackbarMessageDisplay(message);
+                    SnackbarMessageDisplay(message,1500);
                 }
             }
             if (e.Key == Key.Enter)
             {
                 if (TextBoxNameInput.Text == "")
                 {
-                    SnackbarMessageDisplay("A username is required in order to login");
+                    SnackbarMessageDisplay("A username is required in order to login",1500);
                     e.Handled = true;
                 }
                 //fire the clickbutton in order to pop DialogBox;
@@ -102,8 +117,87 @@ namespace LibraryServer
             {
                 TextBoxNameInput.Text = "";
                 var message = "Maximum character limit is " + MaximumTextCharacters.ToString() + "! The name you tried to enter exceeds the allowed limit!";
-                SnackbarMessageDisplay(message);
+                SnackbarMessageDisplay(message,1500);
             }
+        }
+        //when DialogHostLogIn is opened, execute the following code
+        private void DialogHostLogIn_DialogOpened(object sender, DialogOpenedEventArgs eventArgs)
+        {
+            TextBoxUserName.Text = TextBoxNameInput.Text;
+            //Keyboard.Focus(PasswordBoxUserPassword);
+        }
+
+        private void ButtonCancelDialogHost_Click(object sender, RoutedEventArgs e)
+        {
+            AcceptButtonIsPressed = false;
+            ButtonCancelDialogHost.Command.Execute(null);
+
+        }
+
+
+        private void ButtonAcceptDialogHost_Click(object sender, RoutedEventArgs e)
+        {
+            AcceptButtonIsPressed = true;
+            ButtonAcceptDialogHost.Command.Execute(null);
+
+        }
+
+        //When te DIalogHostLogIn is closing, execute the followiing code
+        private void DialogHostLogIn_DialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            //if the AcceptButton was pressed, do the folowing else do the other
+            if (AcceptButtonIsPressed)
+            {
+                var message = "Succesfully logged in!";
+                SnackbarMessageDisplay(message,2000);
+            }
+            else
+            {
+                TextBoxNameInput.Text = ""; //reset TextBoxNameInput
+                var message = "\"Cancel\" was pressed by the user";
+                SnackbarMessageDisplay(message,2000);
+            }
+            AcceptButtonIsPressed = false; //make sure it's false, whatever it happens
+        }
+
+        private void TextBoxUserName_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (TextBoxUserName.Text.Length > MaximumTextCharacters)
+            {
+                if (!(e.Key == System.Windows.Input.Key.Delete ||
+                    e.Key == System.Windows.Input.Key.Back ||
+                    e.Key == System.Windows.Input.Key.Left ||
+                    e.Key == System.Windows.Input.Key.Right ||
+                    e.Key == System.Windows.Input.Key.A && Keyboard.Modifiers == ModifierKeys.Control ||
+                    e.Key == Key.V && Keyboard.Modifiers == ModifierKeys.Control ||
+                    e.Key == Key.C && Keyboard.Modifiers == ModifierKeys.Control ||
+                    e.Key == Key.X && Keyboard.Modifiers == ModifierKeys.Control ||
+                    e.Key == Key.End || e.Key == Key.Home || e.Key == Key.Tab || e.Key == Key.Enter
+                    ))
+                {
+                    e.Handled = true;
+                    //Because of knwon Issue, can not be called multiple times in short timeSpan
+                    if (tickCount>2)
+                    {
+                        
+                        var message = "Maximum character  is " + MaximumTextCharacters.ToString() + "!";
+                        //var messageQueue = SnackbarDialogHost.MessageQueue;
+                        ////find out what this shit does
+                        //Task.Factory.StartNew(() => messageQueue.Enqueue(message));
+
+
+                        var messageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(1000));
+                        SnackbarDialogHost.MessageQueue = messageQueue;
+                        SnackbarDialogHost.MessageQueue.Enqueue(message);
+                        tickCount = 0;
+                    }
+                }
+            }
+        }
+
+        private void PasswordBoxUserPassword_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
