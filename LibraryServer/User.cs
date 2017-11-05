0979dfe87fe271d8805ba8b04b970160e10406a0
Stefan <psa9710@gmail.com>
2017-11-05 13:29:14 +0200

    Added User Class

---
+++ LibraryServer/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryServer
+{
+    class User : Person
+    {
+        public bool isTeacher { get; private set; }
+
+        public override void AddBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
